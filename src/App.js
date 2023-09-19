b473db643a7940671d56625a477ce24c2ff4f676
Deus <amadeusrm97@gmail.com>
2023-09-19 18:05:20 -0400

    test2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <NavBar />
+
+    
   );
 }
 
