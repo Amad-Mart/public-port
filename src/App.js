629b490b4907c8ca3fd8107c1806ed46057d72fa
Deus <amadeusrm97@gmail.com>
2023-09-19 19:06:50 -0400

    t6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
 
 function App() {
   return (
-    <NavBar />
+    <div className="App">
+      <NavBar />
+      <Banner />
+      
+    </div>
   );
 }
 
