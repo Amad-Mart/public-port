e56354ecc3601d2e3741d83dcd55599947b93d16
Deus <amadeusrm97@gmail.com>
2023-09-19 17:53:31 -0400

    test1

---
+++ src/components/NavBar.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+
+
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  });
+  
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value); 
+  }  
+  
+  return (
+    <Navbar expand="md" className={scrolled ? "scrolled": ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+         
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#link" className={activeLink === 'skills' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('project')}>Projects</Nav.Link>
+          </Nav>
+            <span className="navbar-text">
+            <div className="social-icon">
+                
+              </div>
+              <button className="vvd" onClick={() => console.log("connect")}>
+                <span>Let's Connect</span>
+              </button>
+            </span>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
