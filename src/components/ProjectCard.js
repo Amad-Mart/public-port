c310b9adc9f4fe7254207b215782907c6e3cd7d4
Deus <amadeusrm97@gmail.com>
2023-10-05 15:00:25 -0400

    update no proj

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,18 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, linkTo }) => {
   return (
-    <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+    <Col size={12} sm={3} md={4}>
+      <a href={linkTo} target="blank" className="proj-link"> {/* Use an anchor tag with href */}
+        <div className="proj-imgbx">
+          <img src={imgUrl} className="projectIMG" alt={title} />
+          <div className="proj-txtx">
+            <h4 className="ProjectCardTitle">{title}</h4>
+            <span className="ProjectCardDescription">{description}</span>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
-  )
-}
+  );
+};
+
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,46 +1,59 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import React, { useEffect } from "react";
+import { Container, Row, Col, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
+import RiversideHome from "../assets/img/Riversidemfg-Homepage.PNG";
+import MckesssonHome from "../assets/img/mckessson-Homepage.PNG";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-//import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import TrackVisibility from "react-on-screen";
+import $ from 'jquery';
 
 export const Projects = () => {
-
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Riverside Homepage",
+      description: "HTML, CSS, JS, PHP, and Wordpress",
+      imgUrl: RiversideHome,
+      linkTo: "https://www.riversidemfg.com/",
+    },
+    {
+      title: "McKesson Homepage",
+      description: "HTML, CSS, JS, PHP, and Wordpress",
+      imgUrl: MckesssonHome,
+      linkTo: "https://www.mckesson.com/",
     },
     {
       title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
+      //description: "Design & Development",
       imgUrl: projImg3,
+      linkTo: "https://www.riversidemfg.com/",
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+    // ... other projects
   ];
+
+  useEffect(() => {
+    // Function to adjust image heights
+    const adjustImageHeights = () => {
+      $('.projectIMG').each(function () {
+        var imgWidth = $(this).width();
+        var minDesiredHeight = imgWidth * 0.8; // 20% less than the width
+        var maxDesiredHeight = imgWidth * 0.9; // 20% less than the width
+        $(this).css('min-height', minDesiredHeight + 'px');
+        $(this).css('max-height', maxDesiredHeight + 'px');
+      });
+    };
+
+    // Call the function when the component mounts
+    adjustImageHeights();
+
+    // Optionally, you can add a resize event listener to adjust the heights on window resize
+    window.addEventListener('resize', adjustImageHeights);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', adjustImageHeights);
+    };
+  }, []); // Empty dependency array means this effect runs once when the component mounts
 
   return (
     <section className="project" id="projects">
@@ -48,51 +61,37 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>Here is a collection of projects I have worked on to showcase my experience as a Web Developer. Click a project to get a better view</p><br />
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => (
+                            <ProjectCard
+                              key={index}
+                              {...project}
+                            />
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="section">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="Background" />
     </section>
-  )
-}
+  );
+};
